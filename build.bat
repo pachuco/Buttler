006a1f744295492f47aaab6d19e7f9d1870e3487
pachuco <ifail@life.net>
2021-07-11 16:21:20 +0300

    Update build script and .gitignore

--- build.bat
+++ build.bat
@@ -6,17 +6,26 @@ call getcomp.bat rosbe
 set buttiolocation=C:\p_files\prog\_proj\CodeCocks\buttio
 
 set opts=-std=c11 -mconsole -Os -s -Wall -Wextra
-set linkinc=-I%buttiolocation%\src\ -L%buttiolocation%\bin\ -Llib\bin\ -Ilib\enet-1.3.17\include
-set linkinc=%linkinc% -lenet -lbuttio
+set linkinc=-I%buttiolocation%\src\ -L%buttiolocation%\bin\ -Llib\bin\ -Ilib\enet-1.3.17\include -Ilib\ini_rw-master -Ilib\miniaudio-0.10.35
+set linkinc=%linkinc% -linirw -lminiaudio -lenet -lbuttio
 set linkinc=%linkinc% -lwinmm -lWs2_32
 set errlog=.\buttserve_err.log
 
 xcopy "%buttiolocation%\bin\buttio.sys" .\bin\ /c /Y
-del .\bin\buttserve.exe
-gcc -o .\bin\buttserve.exe src\buttler-server.c %opts% %linkinc% 2> %errlog%
-del .\bin\buttclient.exe
-gcc -o .\bin\buttclient.exe src\buttler-client.c %opts% %linkinc% 2> %errlog%
+call :comp .\bin\buttserve.exe src\buttler-server.c "%opts% %linkinc%" buttserve_err.log
+call :comp .\bin\buttclient.exe src\buttler-client.c "%opts% %linkinc%" buttclient_err.log
 
+exit /B 0
+
+
+:comp
+echo compiling %1
+del %1
+gcc -o %1 %2 %opts% %linkinc% 2> %4
+call :checkerr
+exit /B 0
+
+:checkerr
 IF %ERRORLEVEL% NEQ 0 (
     echo oops!
     notepad %errlog%
@@ -24,3 +33,4 @@ IF %ERRORLEVEL% NEQ 0 (
 )
 for %%R in (%errlog%) do if %%~zR lss 1 del %errlog%
 :end
+exit /B 0
