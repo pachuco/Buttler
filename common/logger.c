b382b8017079edf608fcdce302301fbb92481d11
77-777 <thomwalker732@mail.com>
2023-01-03 23:14:50 +0000

    Implementing logger. Still have some more to do.

--- common/logger.c
+++ common/logger.c
@@ -4,32 +4,47 @@
 
 #include "logger.h"
 
-LOGGER * factory_create_logger() {
+LOGGER * factory_create_logger(int TYPE) {
 	return (struct LOGGER*)malloc(sizeof(LOGGER));
 }
 
-int log_info() {
-
+int log_info(char * message) {
+	if (ENABLED_LOGGING == 0) {
+		//log
+		if (
+	}
 }
 
 
-int log_error() {
-
+int log_error(char * message) {
+	if (ENABLED_LOGGING == 0) {
+                //log
+        }
 }
 
 
-int log_warning() {
-
+int log_warning(char * message) {
+	if (ENABLED_LOGGING == 0) {
+                //log
+        }
 }
 
 char * get_current_time_str() {
-
+	//TODO.
 }
 
-int set_logger() {
-
+void set_logger(LOGGER * newLogger) {
+	global_logger = newLogger;
 }
 
-int get_logger() {
+LOGGER * get_logger() {
+	return global_logger;
+}
 
+void enable_logger() {
+	ENABLED_LOGGING = 0;
+}
+
+void disable_logger() {
+	ENABLED_LOGGING = 1;
 }
--- include/logger.h
+++ include/logger.h
@@ -6,19 +6,29 @@
 #define LOGGER_H
 
 // Structures:
-enum LOG = { FILE, STDOUT, STDERR };
+
+int ENABLED_LOGGING = -1;
+int TYPE_OF_LOGGER = -1;
+
+enum LOG = { FILE = 1, STDOUT = 2, STDERR = 3 };
+
+FILE * log_file = NULL;
+char *log_file_path = "";
 
 // Signatures:
 
-LOGGER * factory_create_logger();
+LOGGER * factory_create_logger(int TYPE);
 
-int log_info();
-int log_error();
-int log_warning();
+int log_info(char * message);
+int log_error(char * message);
+int log_warning(char * message);
 
 char * get_current_time_str();
 
-int set_logger();
-int get_logger();
+void set_logger(LOGGER * newLogger);
+LOGGER * get_logger();
+
+void enable_logger();
+void disable_logger();
 
 #endif
