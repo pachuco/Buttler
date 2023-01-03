71f424270dfaf086ae92ac6fc7b6099f1d867db2
pachuco <ifail@life.net>
2023-01-03 13:54:28 +0200

    Delete bloody everything.

--- lib/miniaudio-0.10.35/miniaudio.c
+++
@@ -1,9 +0,0 @@
-#define MINIAUDIO_IMPLEMENTATION
-#define MA_NO_DECODING
-#define MA_NO_ENCODING
-#define MA_NO_WAV
-#define MA_NO_FLAC
-#define MA_NO_MP3
-#define MA_NO_GENERATION
-
-#include "miniaudio.h"
