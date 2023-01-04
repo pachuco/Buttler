c28a809edadeb986d8319cdae10af0f25bfd036e
77-777 <thomwalker732@mail.com>
2023-01-04 01:48:00 +0000

    More enet work.

--- include/enet_engine.h
+++ include/enet_engine.h
@@ -10,34 +10,49 @@
 #define BUFFER_SIZE 4096
 
 enum CONNECTION_STATE = { CONNECTED = 0, DISCONNECTED = 1, UNRESPONSIVE = 2, POLLED = 3 };
+enum PACKET_STATE = { DELIVERED = 0, UNDELIVERED = 1, DELIVERING = 2 };
 
 typedef struct ENET_SERVER {
 	ENetAddress * addr,
 	ENetHost * host_server,
 
-	int bytes_written,
-	int bytes_read,
+	long bytes_written,
+	long bytes_read,
 
 	char send_buffer[BUFFER_SIZE],
 	char recv_buffer[BUFFER_SIZE],
 
-	int state,
-	int last_response
+	int connection_state,
+	long last_response
 } ENET_INFO;
 
 typedef struct ENET_CLIENT {
 	ENetAddress * addr,
 	ENetHost * host_client,
 
-	int bytes_written,
-	int bytes_read,
+	long bytes_written,
+	long bytes_read,
 
 	char send_buffer[BUFFER_SIZE],
 	char recv_buffer[BUFFER_SIZE],
 
-	int state,
-	int last_response
+	int connection_state,
+	long last_response
 } ENET_CLIENT;
+
+typedef struct ENET_PACKET {
+	ENetPacket packet,
+
+	int packet_state,
+	long packet_size
+} ENET_PACKET;
+
+// the main io service.
+int enet_poll_run();
+
+
+
+
 
 int enet_close();
 int enet_listen();
