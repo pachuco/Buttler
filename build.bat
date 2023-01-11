a7578b406a85cebf23ec5c636edeeefdab439aef
77-777 <thomwalker732@mail.com>
2023-01-11 02:01:10 +0000

    Close to finalizing build script.

--- build.bat
+++ build.bat
@@ -1,37 +1,157 @@
 @echo off
+SETLOCAL
 
 set CC=gcc.exe
 set LNK=link.exe
 
-:: =================
 
+
+
+
+:: ========================================
+:: ========================================
+:: ========================================
+
+goto main
+
+:: ========================================
+:: ========================================
+:: ========================================
+
+
+
+
+
+:: =====================
 :all
-	call common
-	call server
-	call client
-	call link
+	call :common
+	call :server
+	call :client
+	call :link
 
-:: =================
+	EXIT /B 0
+:: =====================
 
-:: set output directory to /bin for all these commands below
 
+
+
+
+:: =====================
 :client
-	%CC% -Wall -I include/ src/fm_client/client.c src/fm_client/main.c
 
+	echo:
+	echo "Attempting build of client module..."
+	echo:
+
+	echo "...Client.	skip.
+	:: %CC% -Wall -I include/ src/fm_client/client.c src/fm_client/client.c
+
+	echo "...Main.	skip.
+	:: %CC% -Wall -I include/ src/fm_client/main.c src/fm_client/main.c
+
+	echo "Finished."
+	echo:
+
+	echo:
+	EXIT /B 0
+:: =====================
+
+
+
+
+
+:: =====================
 :server
-	%CC% -Wall -I include/ src/fm_server/server.c src/fm_server/main.c src/fm_server/fm_device.c
 
+	echo:
+	echo "Attempting build of server module..."
+	echo:
+
+	echo "...Server.	skip.
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/server.c -o bin/server.o
+
+
+	echo "...Portio.	skip.
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/portio.c -o bin/portio.o
+
+
+	echo "...Main.	skip.
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/main.c -o bin/main.o
+
+	echo "Finished."
+	echo:
+
+	echo:
+	EXIT /B 0
+:: =====================
+
+
+
+
+
+:: =====================
 :common
-	%CC% -Wall -I include/ common/thread.c common/config.c common/cipher.c common/client.c common/logger.c common/enet_engine.c
+	echo:
+	echo "Attempting build of common modules..."
+	echo:
 
+	echo "...Logger."
+	%CC% -Wall -g -mwin32 -I include/ -c common/logger.c -o bin/logger.o
+
+
+	echo "...Config.""
+	%CC% -Wall -g -mwin32 -I include/ -c common/config.c -o bin/config.o
+
+
+	echo "...Cipher."
+	%CC% -Wall -g -mwin32 -I include/ -c common/cipher.c -o bin/cipher.o
+
+
+	echo "...Thread.		skip."
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/thread.c -o bin/thread.o
+
+
+	echo "...Enet_engine. 	skip."
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/enet_engine.c -o bin/enet_engine.o
+
+
+	echo "...Client. 		skip.
+	:: %CC% -Wall -g -mwin32 -I include/ -c common/client.c -o bin/client.o
+
+	echo:
+	echo "Finished."
+
+	echo:
+	EXIT /B 0
+:: =====================
+
+
+
+
+
+:: =====================
 :link
-	exit
-	gcc -l all objects and libraries to produce exe.
+	echo:
+	echo "Unimplemented."
+	echo:
+
+	:: gcc -l all objects and libraries to produce exe.
 	:: internal dependencies
 
-	gcc - rest of libraries.
+	:: gcc - rest of libraries.
 	:: external dependencies
 
-:: =================
+	echo:
+	EXIT /B 0
+:: =====================
 
-call :%1%
+
+
+
+
+:: =====================
+:main
+	call :%1
+
+	EXIT /B %ERRORLEVEL% 
+:: ====================
